68897899e8e344740a134359deee7bd916f84f44
Jaipal-Github <reddyjaipal2001@gmail.com>
2024-04-09 11:12:58 +0530

    two more files created

---
+++ FirstProject/src/com/learn/branch/Demo.java
@@ -0,0 +1,8 @@
+package com.learn.branch;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Demo class main method");
+	}
+}
---
+++ FirstProject/src/com/learn/branch/Display.java
@@ -0,0 +1,8 @@
+package com.learn.branch;
+
+public class Display {
+
+	public static void main(String[] args) {
+		System.out.println("Display class main method");
+	}
+}
